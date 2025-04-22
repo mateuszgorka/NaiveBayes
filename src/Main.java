658e42e0526d15a16dd4a822bd22d768f2afe3a5
mateuszgorka <gorkamateusz405@gmail.com>
2025-04-22 14:54:47 +0200

    Class merge

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
         NaiveBayesClassifier nv = new NaiveBayesClassifier();
         try {
-            nv.train("data/agaricus-lepiota.data");
+            NaiveBayesClassifier classifier = new NaiveBayesClassifier(); // Jedna instancja
+            classifier.train("data/agaricus-lepiota.data"); // Trenujemy instancję
+
+            List<String[]> testData = Reading.loadData("data/agaricus-lepiota.test.data");
+
+            NaiveBayesClassifier.ClassificationResults result = classifier.test(testData);
+            System.out.println(result);
 
 
-            String[] features = {"e", "x", "s", "n", "t"};
-            nv.classify(features);
+
 
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/NaiveBayesClassifier.java
+++ src/NaiveBayesClassifier.java
@@ -65,7 +65,7 @@ public class NaiveBayesClassifier {
         // -> najpierw prawd apriori
         double apriori = (double) classCounts.getOrDefault(label, 0) / totalExamples;
         // ->>>>>>>>> Np., dla label="e" i totalExamples = 10, a classCounts.get("e") = 6, to prior = 6/10 = 0.6
-
+//        System.out.println("Apriori dla " + label + ": " + apriori);
         // okej teraz iterujemy po tablicy
 
 
@@ -91,11 +91,15 @@ public class NaiveBayesClassifier {
             }
 
             // -> i teraz liczymy prawd. warunkowe z wygładzeniem laplace
-            double condProbality = (matchingFeatureVal + 1) / (totalFeatureQuantity + featureValMap.size());
+            double condProbality = featureValMap.isEmpty() ?
+                    1.0 / numFeatures :
+                    (double) (matchingFeatureVal + 1) / (totalFeatureQuantity + featureValMap.size());
             //  -> Np  jeśli featureValue="x", label="e", i=1, matchingFeatureCount=3, totalFeatureCount=6 (3+3)
             // , a featureValueCounts.size=2 to : (3+1)/(6+2)=4/8=0.5
+
             iloczynPrawdWarunkowych *= condProbality;
         }
+
         return iloczynPrawdWarunkowych * apriori;
     }
 
@@ -141,8 +145,56 @@ public class NaiveBayesClassifier {
         double f1Score = 2 * (precision * recall) / (precision + recall);
 
 
-        return new ClassificationResults(correctPredictions, totalPredictions, accuracy, precision, recall, f1Score,truePositives,trueNegatives,falsePositives,falseNegatives);
+        return new ClassificationResults(correctPredictions, totalPredictions, accuracy, precision, recall, f1Score, truePositives, trueNegatives, falsePositives, falseNegatives);
     }
 
+
+    static class ClassificationResults {
+
+        public int correctPredictions;
+        public int totalPredictions;
+        public double accuracy;
+        public double precision;
+        public double recall;
+        public double f1_Score;
+        public int truePositives;
+        public int trueNegatives;
+        public int falsePositives;
+        public int falseNegatives;
+
+
+
+        public ClassificationResults(int correctPredictions, int totalPredictions, double accuracy, double precision, double recall, double f1Score, int truePositives, int trueNegatives, int falsePositives, int falseNegatives) {
+            this.correctPredictions = correctPredictions;
+            this.totalPredictions = totalPredictions;
+            this.accuracy = accuracy;
+            this.precision = precision;
+            this.recall = recall;
+            this.f1_Score = f1Score;
+            this.truePositives = truePositives;
+            this.trueNegatives = trueNegatives;
+            this.falsePositives = falsePositives;
+            this.falseNegatives = falseNegatives;
+
+        }
+
+
+        @Override
+        public String toString() {
+            return "Wyniki klasyfikacji:\n" +
+                    "Przetworzonych przykładów: " + totalPredictions + "\n" +
+                    "Poprawnie sklasyfikowanych: " + correctPredictions + "\n" +
+                    String.format("Accuracy: %.2f%%\n", accuracy * 100) +
+                    String.format("Precision: %.2f%%\n", precision * 100) +
+                    String.format("Recall: %.2f%%\n", recall * 100) +
+                    String.format("F1-score: %.2f%%\n", f1_Score * 100) +
+                    "\nMacierz pomyłek:\n" +
+                    "             | Przewidywane e | Przewidywane p |\n" +
+                    "-------------+----------------+----------------+\n" +
+                    String.format("Prawdziwe e |       %6d    |       %6d    |\n", truePositives, falseNegatives) +
+                    "-------------+----------------+----------------+\n" +
+                    String.format("Prawdziwe p |       %6d    |       %6d    |\n", falsePositives, trueNegatives);
+        }
+    }
 
 }
